abc52f3a3c05e50e886b7d998d17a31aaeb097ab
MakhmudSD <makhwork15@gmail.com>
2025-04-15 09:22:03 +0900

    fix: DOM logic developed

--- src/app/App.test.tsx
+++ src/app/App.test.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import { Provider } from 'react-redux';
-import { store } from './store';
-import App from './App';
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./MaterialTheme";
+import App from "./App";
+import ReactDOM from "react-dom";
+import reportWebVitals from "../reportWebVitals";
 
-test('renders learn react link', () => {
-  const { getByText } = render(
+ReactDOM.render(
+  <React.StrictMode>
     <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
       <App />
     </Provider>
-  );
+  </React.StrictMode>,
+  // eslint-disable-next-line testing-library/no-node-access
+  document.getElementById("root")
+);
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
-});
+reportWebVitals();
