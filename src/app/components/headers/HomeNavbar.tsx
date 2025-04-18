59fa0d31622baf92e7b0eaa2ada6c195c564d263
MakhmudSD <makhwork15@gmail.com>
2025-04-18 13:54:22 +0900

    feat: components integrated

---
+++ src/app/components/headers/HomeNavbar.tsx
@@ -0,0 +1,5 @@
+import { Container } from "@mui/material";
+
+export default function HomeNavbar() {
+    return <Container>HomeNavbar</Container>
+}
---
+++ src/app/components/headers/OtherNavbar.tsx
@@ -0,0 +1,5 @@
+import { Container } from "@mui/material";
+
+export default function OtherNavbar() {
+    return <Container>OtherNavbar</Container>
+}
