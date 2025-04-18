c2a0ee0fd13e79ff3535f92445baa97c53e18ffc
MakhmudSD <makhwork15@gmail.com>
2025-04-18 13:53:47 +0900

    feat: client routing developed

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,44 @@
-import React from "react";
-import "../css/app.css";
-import { Box, Button, Container, Stack, Typography } from "@mui/material";
-import { RippleBadge } from "./MaterialTheme/styled";
+import { Route, Switch, useLocation } from "react-router-dom";
+import  HomePage  from "./screens/homePage";
+import OrdersPage  from "./screens/ordersPage";
+import  ProductsPage  from "./screens/productsPage";
+import UserPage  from "./screens/userPage";
+import HomeNavbar  from "./components/headers/HomeNavbar";
+import OtherNavbar  from "./components/headers/OtherNavbar";
+import Footer from "./components/footer";
+// import "../css/app.css";
+// import "../css/navbar.css";
+// import "../css/footer.css";
+import  HelpPage  from "./screens/helpPage";
+
 
 function App() {
+  const location = useLocation(); // returns object
   return (
-    <Container sx={{ background: "orange" }}>
-      <Stack flex-direction={"column"}>
-        <Box sx={{ my: 4 }}>
-          <Typography variant="h4" component={"h4"}>
-            Create React App on TypeScript with Redux
-          </Typography>
-        </Box>
-        <Box>
-          <RippleBadge badgeContent={4}>
-            <Button variant="contained">Contained</Button>
-          </RippleBadge>
-        </Box>
-      </Stack>
-    </Container>
+    <>
+      {location.pathname === "/" ? <HomeNavbar /> : <OtherNavbar />}
+      <Switch>
+        {/** checks the path and directs us to the related page */}
+        <Route path="/products">
+          <ProductsPage />
+        </Route>
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+        <Route path="/member-page">
+          <UserPage />
+        </Route>
+        <Route path="/help">
+          <HelpPage />
+        </Route>
+        <Route path="/">Home
+          {" "}
+          {/** route / must be put at the end to provide correct switching */}
+          <HomePage />
+        </Route>
+      </Switch>
+      <Footer />
+    </>
   );
 }
 
