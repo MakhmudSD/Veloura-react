099baeb19f228a0a5d8a25b703ee52a3d4ae096b
MakhmudSD <makhwork15@gmail.com>
2025-08-13 21:44:49 +0900

    fix: deploy fixed

--- deploy.sh
+++ deploy.sh
@@ -5,15 +5,9 @@ git reset --hard
 git checkout master
 git pull origin master
 
-npm i
-npm run build
-pm2 start process.config.js --env production
-
-# # DEVELOPMENT
-# git reset --hard
-# git checkout develop
-# git pull origin develop
-
-# npm i
-# pm2 start "npm run start:dev" --name=VELOURA-PERFUME
+npm i yarn -g
+yarn global add serve
+yarn
+yarn run build
+pm2 start "yarn run start:prod" --name=Veloura-react
 
